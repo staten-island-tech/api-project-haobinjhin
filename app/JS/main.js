71c3d6b945388b2b5d82396595f50e8a2c84dbcb
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-11-25 12:32:14 -0500

    huhuiun

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,11 +1,18 @@
 const URL = "https://dogapi.dog/api/v2/breeds";
 
+DOM = {
+    textbreed: document.getElementById("breed/")
+}
+
+
 async function getData(URL){
 
     try {
         const response = await fetch(URL);
         const data = await response.json();
-        console.log(data);
+        if (document.getElementById("breed").textContent = "Caucasian Shepherd Dog")
+            {document.getElementById("api-response").textContent = data.data[0].attributes.description}
+        console.log(data.data[1].attributes.description)
     } catch (error) {
         console.log("error")
     }
