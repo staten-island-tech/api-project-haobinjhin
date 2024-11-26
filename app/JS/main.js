5fcee32e9620ca63a52425865fd0a02d5354b555
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-11-26 12:31:00 -0500

    dfdsfdfsdf

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,7 +1,8 @@
 const URL = "https://dogapi.dog/api/v2/breeds";
 
-DOM = {
-    textbreed: document.getElementById("breed/")
+const DOM = {
+    box: document.getElementById("box"),
+    textbreed: document.getElementById("breed"),
 }
 
 
@@ -9,15 +10,24 @@ async function getData(URL){
 
     try {
         const response = await fetch(URL);
-        const data = await response.json();
-        if (document.getElementById("breed").textContent = "Caucasian Shepherd Dog")
-            {document.getElementById("api-response").textContent = data.data[0].attributes.description}
-        console.log(data.data[1].attributes.description)
+        const dogdata = await response.json();
+        console.log(dogdata)
+        dogdata.data.forEach(dog => {makecard(dog.attributes.name)
+    });
+
     } catch (error) {
         console.log("error")
     }
     
 }
 
-
 getData(URL)
+
+function makecard(name){
+    DOM.box.insertAdjacentHTML("beforeend", 
+        `<div class = "card"> 
+        <h1>${name}</h1>
+        </div>`)
+
+}
+
